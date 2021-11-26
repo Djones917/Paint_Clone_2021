42b2df41066fca23a3be9885d7ae62d99a08d20f
Derek <nightwing917@hotmail.com>
2021-11-25 19:24:24 -0700

    called createcanvas function in setting background color

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ let currentColor = '#A51DAB';
 // Setting Background Color
 bucketColorBtn.addEventListener('change', () => {
    bucketColor = `#${bucketColorBtn.value}`;
+   createCanvas();
 });
 
 // // Eraser
